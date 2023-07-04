6a2a9ba2219050062938fb552c065877f7727ec6
Ann Abrajeeva <anuta040786@yandex.ru>
2023-07-04 18:00:34 +0500

    feat: add slider

--- src/js/components/reviews-slider.js
+++ src/js/components/reviews-slider.js
@@ -1,7 +1,6 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 const reviewsSlider = new Swiper('.reviews__slider', {
-  slidesPerView: 'auto',
   slidesPerView: 4,
   pagination: {
     el: ".reviews__slider-pag",
@@ -10,6 +9,28 @@ const reviewsSlider = new Swiper('.reviews__slider', {
   navigation: {
     nextEl: ".reviews__slider-next",
     prevEl: ".reviews__slider-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      centeredSlides: true
+    },
+    728: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+      centeredSlides: false
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      centeredSlides: false
+    },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+      centeredSlides: false
+    }
   },
 });
 
